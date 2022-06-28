1bf12b0ac1e16c328ed065dfdd76d7bad03ed8b3
Hardeepsinh Sodha <41581160+HardeepsinhSodha@users.noreply.github.com>
2022-06-28 14:38:46 +0530

    Create configLogin.py

---
+++ configLogin.py
@@ -0,0 +1,3 @@
+api_key = "jug0majrzABCDED" # set your api_key. # it is fake key 
+# don't have api_key. go to https://developers.kite.trade/ create new app and copy api_key from their.
+# to download hestorical data you have to purchase their plan for that. 
